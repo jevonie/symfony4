f8cce503017012e74e53e161d1a00bcaf895f5a0
jevonie <jevonievillarin@cmu.edu.ph>
2018-02-05 11:53:25 +0800

    first commit

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function index()
+    {
+        $number = mt_rand(0, 100);
+
+        return $this->render('base.html.twig', array(
+            'number' => $number,
+        ));
+    }
+}
