b6883c21d8142a4da2921efe9f67cd420a7455a1
jevonie <jevonievillarin@cmu.edu.ph>
2018-02-06 15:54:44 +0800

    Third Commit

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,13 +4,16 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use App\Entity\Users;
+use App\Form\UserType;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class DefaultController extends Controller
-{
+{   
+    /**
+     * @Route("/", name="home_page")
+     */
     public function index()
     {
         $number = mt_rand(0, 100);
@@ -19,19 +22,48 @@ class DefaultController extends Controller
             'number' => $number,
         ));
     }
-    public function buildForm()
-    {
-        $form = $this->createFormBuilder()
-            ->add('name', TextType::class)
-            ->add('email', TextType::class)
-            ->add('password', TextType::class)
-            ->add('gender', TextType::class)
-            ->add('description', TextType::class)
-            ->add('save', SubmitType::class, array('label' => 'Save'))
-            ->getForm();
+     /**
+     * @Route("/form", name="user_registration")
+     */
+    public function registerAction(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {   
+        $user = new Users();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+
+            $password =  $user->getPlainPassword();
+            $user->setPassword($password);
+
+            // ... perform some action, such as saving the task to the database
+            // for example, if Task is a Doctrine entity, save it!
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('index');
+        }
+
 
         return $this->render('new.html.twig', array(
             'form' => $form->createView(),
         ));
+    }
+
+    /**
+    * @Route("/lists", name="users_show")
+    */
+     public function showAction()
+    {
+        $repository = $this->getDoctrine()->getRepository(Users::class);
+        $user = $repository->findAll();
+
+        return $this->render('list.html.twig', array(
+            'users' => $user,
+        ));
+
     }
 }
--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -3,7 +3,9 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\UserInterface;
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UsersRepository")
  */
@@ -14,12 +16,54 @@ class Users
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
      */
-    // private $id;
+    private $id;
 
-    // add your own fields
-     public function getId()
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $name;
+
+     /**
+     * @ORM\Column(type="string", length=255, unique=true)
+     * @Assert\NotBlank()
+     * @Assert\Email()
+     */
+    private $email;
+
+     /**
+     * @Assert\NotBlank()
+     * @Assert\Length(max=4096)
+     */
+    private $plainPassword;
+
+
+    /**
+     * The below length depends on the "algorithm" you use for encoding
+     * the password, but this works well with bcrypt.
+     *
+     * @ORM\Column(type="string", length=64)
+     */
+    private $password;
+
+
+    /**
+    * @ORM\Column(type="string", length=100)
+    */
+    private $gender;
+
+    /**
+    * @ORM\Column(type="string", length=100)
+    */
+    private $description;
+
+    public function getEmail()
     {
-        return $this->id;
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
     }
 
     public function getName()
@@ -31,4 +75,50 @@ class Users
     {
         $this->name = $name;
     }
+
+    public function getPlainPassword()
+    {
+        return $this->plainPassword;
+    }
+
+    public function setPlainPassword($password)
+    {
+        $this->plainPassword = $password;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+     public function getGender()
+    {
+        return $this->gender;
+    }
+
+    public function setGender($gender)
+    {
+        $this->gender = $gender;
+    }
+        public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+    public function getSalt()
+    {
+        // The bcrypt and argon2i algorithms don't require a separate salt.
+        // You *may* need a real salt if you choose a different encoder.
+        return null;
+    }
+
+    // other methods, including security methods like getRoles()
 }
